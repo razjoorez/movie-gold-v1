323934419e690febee288e805711ca9f05152cc2
reza razjoo <reza.razjoo@yahoo.com>
2024-04-30 23:20:40 +0100

    old and new api

--- src/api/axiossConfig.js
+++ src/api/axiossConfig.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-//new api  http://13.53.37.158:8080/api/v1/movies
+//new api  http://13.53.37.158:8080/api/v1/movies/
 //olpd api  http://localhost:8080
 export default axios.create({
     baseURL:'http://13.53.37.158:8080'
