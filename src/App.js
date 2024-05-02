195822ef8f01f3ee98dedf90b2054819d6a2eb20
Reza Razjoo <reza.razjoo@yahoo.com>
2024-05-02 19:38:45 +0100

    hero

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import api from './api/axiossConfig';
 import { useEffect,useState } from 'react';
 import Layout from './components/Layout';
 import {Routes,Route} from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
   const [movies,setMovies] = useState();
@@ -26,8 +27,8 @@ function App() {
   return (
     <div className="App">
      <Routes>
-      <Route path='/' element={Layout}>
-
+      <Route path='/' element={<Layout/>}>
+      <Route path='/' element={<Home movies={movies}/>}></Route>
       </Route>
      </Routes>
     </div>
